76a7fee730e45a30a5ca49632a736e041a46029d
jgonza40 <jessica_gonzalez1@brown.edu>
2020-06-30 15:26:56 -0700

    able to publish tweet now

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+
+import okhttp3.Headers;
 
 public class ComposeActivity extends AppCompatActivity {
 
@@ -17,6 +25,8 @@ public class ComposeActivity extends AppCompatActivity {
     Button btnTweet;
     TextView tvCounter;
     public static final int MAX_TWEET_LENGTH = 280;
+    public static final String TAG = "ComposeActivity";
+    TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,7 @@ public class ComposeActivity extends AppCompatActivity {
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
         tvCounter = findViewById(R.id.tvCounter);
+        client = TwitterApp.getRestClient(this);
 
         etCompose.addTextChangedListener(new TextWatcher() {
             @Override
@@ -50,7 +61,7 @@ public class ComposeActivity extends AppCompatActivity {
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String tweetContent = etCompose.getText().toString();
+                final String tweetContent = etCompose.getText().toString();
                 if(tweetContent.isEmpty()){
                     Toast.makeText(ComposeActivity.this, "your tweet cannot be empty :/", Toast.LENGTH_LONG).show();
                     return;
@@ -61,6 +72,25 @@ public class ComposeActivity extends AppCompatActivity {
                 }
                 Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
                 //Make API call to twitter
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG, "onSuccess to publish Tweet");
+                        try {
+                            Tweet tweet = Tweet.fromJSON(json.jsonObject);
+                            Log.i(TAG, "Published tweet says: " + tweet.body);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG, "onFailure to publish Tweet", throwable);
+
+                    }
+                });
             }
         });
 
